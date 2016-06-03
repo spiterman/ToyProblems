a06343e73ec42691b0408fda328033fafb25d639
Sergey Piterman <sergeypiterman@gmail.com>
2016-06-02 23:35:00 -0700

    [feat] Start matrix Multiplication

---
+++ June2016/matrixMultiply.js
@@ -0,0 +1,12 @@
+function matrixMultiply(mat1, mat2){
+  if(mat1[0].length !== mat2.length){
+    return console.log("Cannot multiply matrices!");
+  }
+
+}
+
+matrixMultiply([[1]], [1, 3])
+
+var a = [[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]];
