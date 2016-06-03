0b4e53d0b00c8149e2f9fbadae658828978a1e7b
Sergey Piterman <sergeypiterman@gmail.com>
2016-06-02 23:38:28 -0700

    [feat] Start matrix Scaling

--- June2016/matrixMultiply.js
+++ June2016/matrixMultiply.js
@@ -5,8 +5,21 @@ function matrixMultiply(mat1, mat2){
 
 }
 
+function matrixScaling(constant, matrix){
+  for(var i = 0; i< matrix.length; i++){
+    for(var j = 0; j<i.length; j++){
+      matrix[i][j] = constant * matrix[i][j]
+    }
+  }
+  return matrix;
+}
+
+matrixScaling
+
 matrixMultiply([[1]], [1, 3])
 
 var a = [[1, 0, 0],
      [0, 1, 0],
      [0, 0, 1]];
+
+matrixScaling(2, a)
