129049a4636e7751576f0affd4e1d69f930cccaf
Sergey Piterman <sergeypiterman@gmail.com>
2017-10-27 15:33:22 -0700

    (feat) adds parens permutations

---
+++ Algorithms/Recursion/ParensPermutations.js
@@ -0,0 +1,22 @@
+function ParensPermutations(num) {
+  let result = [];
+
+  function recurse(substr, left, right) {
+    if(left === 0 && right === 0) {
+      result.push(substr);
+      return;
+    } if (left === 0){
+      recurse(substr + ')', left, right - 1);
+    } else if(left < right) {
+      recurse(substr + ')', left, right - 1);
+      recurse(substr + '()', left - 1, right - 1);
+    } else {
+      recurse(substr + '(', left - 1, right);
+    }
+  }
+  recurse('', num, num);
+  return result;
+}
+
+let result = ParensPermutations(3);
+console.log(result)
