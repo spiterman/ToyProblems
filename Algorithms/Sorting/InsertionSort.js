ca394ab6cda2caa826d1f362497949c0a958dad5
Sergey Piterman <sergeypiterman@gmail.com>
2017-10-31 16:33:02 -0700

    (feat) adds insertion sort

--- Algorithms/Sorting/InsertionSort.js
+++ Algorithms/Sorting/InsertionSort.js
@@ -1,5 +1,14 @@
 function insertionSort(arr) {
-
+  for(let i = 0; i < arr.length; i++) {
+    let j = i;
+    while(j >= 0 && arr[j-1] > arr[j]) {
+      [arr[j], arr[j-1]] = [arr[j-1], arr[j]];
+      j--;
+    }
+  }
+  return arr;
 }
 
-console.log(insertionSort([3, 2, 1, 6, 4, 8, 10, 39, 28, 57, 25]));
+// console.log(insertionSort([3, 2, 1, 6, 4, 8, 10, 39, 28, 57, 25]));
+
+module.exports = insertionSort;
