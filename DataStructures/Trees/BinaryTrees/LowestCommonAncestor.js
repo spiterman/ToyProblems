ba734fa0808ded03b4c00c6ab613de15c4a7a128
Sergey Piterman <sergeypiterman@gmail.com>
2017-11-07 16:24:32 -0800

    (feat) adds lowest common ancestor

---
+++ DataStructures/Trees/BinaryTrees/LowestCommonAncestor.js
@@ -0,0 +1,67 @@
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function LowestCommonAncestor (root, num1, num2) {
+  let result = -1;
+
+  function ancestorPath (node, target, path) {
+    if(node === null) {
+      return;
+    }
+    if(node.value === target) {
+      path.push(node.value);
+      return true;
+    }
+    if(ancestorPath(node.left, target, path)) {
+      path.push(node.value);
+      return path;
+    }
+    if(ancestorPath(node.right, target, path)) {
+      path.push(node.value);
+      return path;
+    }
+  }
+
+  let arr1 = ancestorPath(root, num1, []) || [];
+  let arr2 = ancestorPath(root, num2, []) || [];
+
+  arr1.reverse();
+  arr2.reverse();
+
+  // console.log(arr1)
+  // console.log(arr2)
+
+  let i = 0;
+
+  while(arr1[i] === arr2[i]) {
+    result = arr1[i];
+    i++;
+  }
+
+  return result;
+}
+
+// Tests
+// const five = new TreeNode(5);
+// const two = new TreeNode(2);
+// const seven = new TreeNode(7);
+// const four = new TreeNode(4);
+// const eight = new TreeNode(8);
+// const nine = new TreeNode(9);
+//
+//
+// five.left = two;
+// five.right = seven;
+// seven.left = four;
+// seven.right = eight;
+// eight.right = nine;
+
+// console.log(LowestCommonAncestor(five, 9, 4))
+
+
+module.exports = LowestCommonAncestor;
