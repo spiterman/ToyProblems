9ba1af538c5f6a8cdb19f1fa12c97f129bf4cefe
Sergey Piterman <sergeypiterman@gmail.com>
2017-12-06 13:45:51 -0800

    (feat) updates logical operators

--- Algorithms/Math/LogicalOperators.js
+++ Algorithms/Math/LogicalOperators.js
@@ -1,25 +1,42 @@
+'use strict';
+
 function ID(p) {
   return p;
 }
 
-function NOT(p) {
-  return !p;
-}
-
-function AND(p, q) {
-  return p && q;
-}
-
-function OR(p, q) {
-  return p || q;
-}
-
 function TAUTOLOGY(p){
-  return OR(p, 1);
+  return true;
 }
 
 function CONTRADICTION(p){
-  return AND(p, 0);
+  return false;
+}
+
+function NOT(p) {
+  if (p) {
+    return true;
+  }
+  return false;
+}
+
+function OR(p, q) {
+  if(p) {
+    return true;
+  }
+  if(q) {
+    return true;
+  }
+  return false;
+}
+
+function AND(p, q) {
+  if(NOT(p)) {
+    return false;
+  }
+  if(NOT(q)) {
+    return false;
+  }
+  return true;
 }
 
 function NAND(p, q) {
@@ -42,22 +59,16 @@ function XOR(p, q) {
   return NOT(EQUALS(p, q));
 }
 
+function XNOR(p, q) {
+  return NOT(XOR(p, q));
+}
+
 function HALFADDER(p, q) {
-  var result = {};
-  result.sum = XOR(p, q);
-  result.carry = AND(p, q);
-  return result;
+  return [XOR(p, q), AND(p, q)];
 }
 
 function FULLADDER(p, q, c) {
-  var result = {};
-  var xor = XOR(p, q);
-  var and = AND(p, q);
-
-  result.sum = XOR(xor, c);
-  result.carry = OR(AND(xor, c), and);
-
-  return result;
+  return [XOR(XOR(p, q), c), OR(AND(XOR(p, q), c), AND(p, q))]
 }
 
 console.log(FULLADDER(0, 0, 0));
@@ -68,5 +79,3 @@ console.log(FULLADDER(0, 1, 1));
 console.log(FULLADDER(1, 0, 1));
 console.log(FULLADDER(1, 1, 0));
 console.log(FULLADDER(1, 1, 1));
-
-
