58bf01e1d3eb111bd01cdd4e3201f2141c7f01c2
Sergey Piterman <sergeypiterman@gmail.com>
2017-12-07 14:31:09 -0800

    (feat) adds alternate bit array sort

--- Algorithms/Sorting/bitArraySort.js
+++ Algorithms/Sorting/bitArraySort.js
@@ -16,4 +16,21 @@ function sortBitArray(arr) {
   return arr;
 }
 
+function sortBits(arr) {
+  let i = 0;
+  let j = 0;
+
+  while(j < arr.length) {
+    if(arr[j] === 0) {
+      arr[j] = 1
+      arr[i] = 0
+      i++
+    }
+    j++
+  }
+  return arr;
+}
+
+// console.log(sortBits([0, 1, 1, 1, 0, 0, 1]))
+
 // console.log(sortBitArray([0, 1, 0, 1, 1, 0]));
