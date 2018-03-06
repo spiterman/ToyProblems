f4e81cba21bb82c7a095bcde9d07cc9f13d276a3
Sergey Piterman <sergeypiterman@gmail.com>
2018-03-06 14:31:27 -0800

    (feat) adds optimized combinations solution

--- Algorithms/Combinatorics/Combinations.js
+++ Algorithms/Combinatorics/Combinations.js
@@ -1,4 +1,4 @@
-function combinations(n, k) {
+function combinations1(n, k) {
   let result= [];
 
   function recurse(start, combos) {
@@ -18,6 +18,37 @@ function combinations(n, k) {
   return result;
 }
 
+// Optimizes space/time by only copying combos when ready to push into result
+// Also saves time by breaking out of recursion when the number of combos can't reach length k
+function combinations2(n, k) {
+  let result= [];
+
+  function recurse(start, combos) {
+    if(combos.length === k) {
+      return result.push(combos.slice());
+    }
+    if(combos.length + (n - start + 1) < k){
+      return;
+    }
+    recurse(start + 1, combos);
+    combos.push(start);
+    recurse(start + 1, combos);
+    combos.pop();
+  }
+
+  recurse(1, []);
+  return result;
+}
+
+
 // combinations(4, 2);
 
-module.exports = combinations;
+console.time("combinations 1")
+combinations1(60, 4)
+console.timeEnd("combinations 1")
+
+console.time("combinations 2")
+combinations2(60, 4)
+console.timeEnd("combinations 2")
+
+// module.exports = combinations;
