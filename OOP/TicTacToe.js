4b92d53a92bc325517c7f308f7d7b93cb09db66a
Sergey Piterman <sergeypiterman@gmail.com>
2018-04-24 20:05:09 -0700

    (feat) fixes small edge case for tie games and refactors to oop

--- OOP/TicTacToe.js
+++ OOP/TicTacToe.js
@@ -1,157 +1,151 @@
-function startGame() {
-  let gameInfo = createGameInfo();
-  play(gameInfo);
-}
-
-function createGameInfo() {
-  let board = createBoard();
-  let currentPlayer = "X";
-  let numberOfRounds = 9;
-  return {board, currentPlayer, numberOfRounds}
-}
-
-function createBoard(){
-  return [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
-}
-
-function play(gameInfo) {
-
-  process.stdout.write("Let's play Tic Tac Toe!\n\n")
-
-  printBoard(gameInfo);
-  printCurrentPlayersTurn(gameInfo);
-  let playRound = roundPlayerFactory(gameInfo);
-
-  process.stdin.on("data", (data) => {
-    let moves = data.toString().trim().split(" ");
-    let row = parseInt(moves[0]);
-    let col = parseInt(moves[1]);
-    process.stdout.write("\n")
-    playRound(row, col);
-  });
-}
-
-function roundPlayerFactory(gameInfo) {
-  function playRound(row, col) {
-    let canPlacePiece = placePiece(gameInfo)(row, col);
-    if(canPlacePiece) {
-      printBoard(gameInfo);
-      printCurrentMove(gameInfo)(row, col)
-      let detectWinner = checkWinCondition(gameInfo);
-      if(detectWinner) {
-        declareWinner(gameInfo);
-      }
-      if(!detectWinner && gameInfo.numberOfRounds <= 0) {
-        declareNoWinners();
-      }
-      switchPlayer(gameInfo)
-      gameInfo.numberOfRounds -= 1;
-      printCurrentPlayersTurn(gameInfo);
-    } else {
-      printInvalidMove(gameInfo);
-    }
+class TicTacToe {
+  constructor() {
+    // Create 3x3 board
+    // Number of Rounds
+    // Current player
+    this.currentPlayer = "X";
+    this.numberOfRounds = 9;
+    this.board = createBoard();
   }
-  return playRound;
-}
 
-function switchPlayer(gameInfo) {
-  if(gameInfo.currentPlayer === "X") {
-    gameInfo.currentPlayer = "O"
-  } else {
-    gameInfo.currentPlayer = "X"
-  }
-}
+  play() {
+    process.stdout.write("Let's play Tic Tac Toe! \n\n");
+    this.printBoard();
+    this.printCurrentPlayersTurn();
 
-function placePiece(gameInfo) {
-  return function(row, col) {
-    if(isNaN(row) || isNaN(col) || row > 2 || row < 0 || col > 2 || col < 0 || gameInfo.board[row][col] === "X" || gameInfo.board[row][col] === "O") {
-      return false;
-    } else {
-      gameInfo.board[row][col] = gameInfo.currentPlayer;
-      return true;
-    }
-  }
-}
+    process.stdin.on("data", (data) => {
+      let moves = data.toString().trim().split(" ");
+      let row = parseInt(moves[0]);
+      let col = parseInt(moves[1]);
+      process.stdout.write("\n");
 
-// Functions to Print Board and Messages
-function printInvalidMove(gameInfo){
-  process.stdout.write(`Invalid Move, ${gameInfo.currentPlayer} goes again \n\n`)
-}
-
-function printCurrentMove(gameInfo) {
-  return function(row, col){
-    process.stdout.write(`An ${gameInfo.currentPlayer} was placed at row ${row}, column ${col} \n\n`);
-  }
-}
-
-function printCurrentPlayersTurn(gameInfo) {
-  process.stdout.write(`It's ${gameInfo.currentPlayer}'s turn \n\n`);
-}
-
-function printBoard(gameInfo) {
-  gameInfo.board.forEach((line) => {
-    line.forEach((square) => {
-      process.stdout.write(`|${square}`);
+      this.playRound(row, col);
     })
-    process.stdout.write("|");
+  }
+
+  printBoard() {
+    this.board.forEach((line) => {
+      line.forEach((square) => {
+        process.stdout.write(`|${square}`);
+      })
+      process.stdout.write("|");
+      process.stdout.write("\n");
+    })
     process.stdout.write("\n");
-  })
-  process.stdout.write("\n");
-}
-
-// Functions to end process and broadcast winner messages
-function declareWinner(gameInfo) {
-  process.stdout.write(`\n${gameInfo.currentPlayer} is the winner! \n`);
-  process.exit();
-}
-
-function declareNoWinners() {
-  process.stdout.write("\nCat's game! \n");
-  process.exit();
-}
-
-// Functions to check for winners
-function checkWinCondition(gameInfo) {
-  // check rows, columns and diagonals
-  if(checkRows(gameInfo) ||
-      checkColumns(gameInfo) ||
-      checkDiagonals(gameInfo)) {
-    return true;
   }
-  return false;
-}
 
-function checkDiagonals(gameInfo){
-  if(gameInfo.board[0][0] === gameInfo.currentPlayer &&
-     gameInfo.board[1][1] === gameInfo.currentPlayer &&
-     gameInfo.board[2][2] === gameInfo.currentPlayer){
-    return true;
+  printCurrentPlayersTurn() {
+    process.stdout.write(`It's ${this.currentPlayer}'s turn \n\n`);
   }
-  if(gameInfo.board[0][2] === gameInfo.currentPlayer &&
-     gameInfo.board[1][1] === gameInfo.currentPlayer &&
-     gameInfo.board[2][0] === gameInfo.currentPlayer){
-    return true;
-  }
-  return false;
-}
 
-function checkRows(gameInfo){
-  for(let row = 0; row < gameInfo.board.length; row++) {
-    if(gameInfo.board[row][0] === gameInfo.currentPlayer && gameInfo.board[row][1] === gameInfo.currentPlayer && gameInfo.board[row][2] === gameInfo.currentPlayer) {
-      return true;
+  playRound(row, col) {
+    if(this.canPlacePiece(row, col)) {
+      // place pice
+      this.printBoard();
+      this.printCurrentMove(row, col);
+      let detectWinner = this.checkWinCondtion();
+      if(detectWinner) {
+        this.declareWinner();
+      }
+      this.decrementRounds();
+      if(!detectWinner && this.numberOfRounds <= 0) {
+        this.declareTie();
+      }
+      this.switchPlayer();
+      this.printCurrentPlayersTurn();
+    } else {
+      this.printInvalidMove();
     }
   }
-  return false;
-}
 
-function checkColumns(gameInfo) {
-  for(let col = 0; col < gameInfo.board.length; col++) {
-    if(gameInfo.board[0][col] === gameInfo.currentPlayer && gameInfo.board[1][col] === gameInfo.currentPlayer && gameInfo.board[2][col] === gameInfo.currentPlayer) {
-      return true;
+  decrementRounds() {
+    this.numberOfRounds -= 1;
+  }
+
+  declareWinner() {
+    process.stdout.write(`\n${this.currentPlayer} is the winner! \n`);
+    process.exit();
+  }
+
+  declareTie() {
+    process.stdout.write(`\nCat's game!\n`);
+    process.exit();
+  }
+
+  printInvalidMove(){
+    process.stdout.write(`Invalid Move, ${this.currentPlayer} goes again \n\n`)
+  }
+
+  switchPlayer() {
+    if(this.currentPlayer === "X") {
+      this.currentPlayer = "O";
+    } else {
+      this.currentPlayer = "X";
     }
   }
-  return false;
+
+  printCurrentMove(row, col) {
+    process.stdout.write(`An ${this.currentPlayer} was placed at row ${row}, column ${col} \n\n`);
+  }
+
+  canPlacePiece(row, col) {
+    if(isNaN(row) || isNaN(col) || row > 2 || row < 0 || col > 2 || col < 0 || this.board[row][col] === "X" || this.board[row][col] === "O") {
+      return false;
+    }
+    this.board[row][col] = this.currentPlayer;
+    return true;
+  }
+
+  checkWinCondtion() {
+    if(this.checkRows() || this.checkColumns() || this.checkDiagonals()) {
+      return true;
+    }
+    return false;
+  }
+
+  checkDiagonals() {
+    if(this.board[0][0] === this.currentPlayer &&
+       this.board[1][1] === this.currentPlayer &&
+       this.board[2][2] === this.currentPlayer) {
+        return true;
+      }
+    if(this.board[0][2] === this.currentPlayer &&
+       this.board[1][1] === this.currentPlayer &&
+       this.board[2][0] === this.currentPlayer) {
+        return true;
+      }
+    return false;
+  }
+
+  checkRows() {
+    for(let row = 0; row < this.board.length; row++) {
+      if(this.board[row][0] === this.currentPlayer && this.board[row][1] === this.currentPlayer && this.board[row][2] === this.currentPlayer) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkColumns() {
+    for(let col = 0; col < this.board.length; col++) {
+    if(this.board[0][col] === this.currentPlayer && this.board[1][col] === this.currentPlayer && this.board[2][col] === this.currentPlayer) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
-// Runs the Game
-startGame();
+function createBoard() {
+  return [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+}
+
+let game = new TicTacToe();
+
+game.play();
+
+/*
+
+Separate the update board from is valid move
+Decrementing the number of rounds before checking win condtion
+*/
