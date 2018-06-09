ff18267cf8471447d28d620d915a491867982d37
Sergey Piterman <sergeypiterman@gmail.com>
2018-06-08 17:34:11 -0700

    (feat) adds matrix

--- DataStructures/Matrices/Matrix.js
+++ DataStructures/Matrices/Matrix.js
@@ -1,26 +1,122 @@
-function Matrix(rows, columns, fill) {
-  this.data = [];
-
-  if(fill === undefined){
-    fill = 0;
+class Matrix {
+  constructor(m, n) {
+    this.m = m;
+    this.n = n;
+    this.storage = [];
+    for(let i = 0; i < m; i++) {
+      this.storage.push(new Array(n).fill(0));
+    }
   }
 
-  var row = [];
-  for(var i = 0; i < columns; i++){
-    row.push(fill);
+  isValidPosition(i, j) {
+    if(i < 0 || j < 0 || i >= this.m || j >= this.n) {
+      return false
+    }
+    return true
   }
-  for(var j = 0; j < rows; j++){
-    this.data.push(row.slice())
+
+  insert(i, j, val) {
+    if(this.isValidPosition(i, j)) {
+      this.storage[i][j] = val
+      return true;
+    }
+    return false;
   }
+
+  retrieve(i, j) {
+    if(this.isValidPosition(i, j)) {
+      return this.storage[i][j]
+    }
+    return -Infinity;
+  }
+
+  scale(factor) {
+    for(let i = 0; i < this.m; i++) {
+      for(let j = 0; j < this.n; j++) {
+        let newVal = this.retrieve(i, j) * factor;
+        this.insert(i, j, newVal);
+      }
+    }
+  }
+
+  fill(val) {
+    for(let i = 0; i < this.m; i++) {
+      for(let j = 0; j < this.n; j++) {
+        this.insert(i, j, val)
+      }
+    }
+  }
+
+  flatten() {
+    let result = [];
+    for(let i = 0; i < this.m; i++) {
+      for(let j = 0; j < this.n; j++) {
+        result.push(this.storage[i][j]);
+      }
+    }
+    return result;
+  }
+
+
+  transpose() {
+    let newMatrix = new Matrix(this.n, this.m);
+    for(let i = 0; i < this.m; i++) {
+      for(let j = 0; j < this.n; j++) {
+        let val = this.retrieve(i, j);
+        newMatrix.insert(j, i, val);
+      }
+    }
+    return newMatrix;
+  }
+
+  multiply(matrix) {
+    if(this.n !== matrix.m) {
+      return null;
+    }
+    let newMatrix = new Matrix(this.m, matrix.n);
+    for(let i = 0; i < newMatrix.m; i++) {
+      for(let j = 0; j < newMatrix.n; j++) {
+
+        let newVal = 0;
+
+        for(let k = 0; k < this.n; k++) {
+          newVal += this.retrieve(i, k) * matrix.retrieve(k, j);
+        }
+
+        newMatrix.insert(i, j, newVal);
+      }
+    }
+
+  return newMatrix;
+  }
+
+
 }
 
-Matrix.prototype.insert = function(row, column, value){
-  this.data[row][column] = value;
-}
+let matrix1 = new Matrix(2, 3)
+// console.log(matrix.storage);
 
+matrix1.insert(0, 0, 5)
+matrix1.insert(0, 1, 4)
+matrix1.insert(0, 2, 6)
+matrix1.insert(1, 0, 7)
+matrix1.insert(1, 1, 8)
+matrix1.insert(1, 2, 9)
+// console.log(matrix1.storage);
 
+let matrix2 = new Matrix(3,2)
+// console.log(matrix.storage);
 
-var m = new Matrix(3, 4);
-m.insert(1, 1, 10);
+matrix2.insert(0, 0, 1)
+matrix2.insert(1, 0, 1)
+matrix2.insert(2, 0, 1)
 
-console.log(JSON.stringify(m.data))
+matrix2.insert(0, 1, 2)
+matrix2.insert(1, 1, 2)
+matrix2.insert(2, 1, 2)
+
+// console.log(matrix2.storage);
+
+console.log(matrix1.multiply(matrix2).storage)
+
+// console.log(matrix.transpose().storage)
