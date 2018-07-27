67a9c4645d9976e82b768a3d6e7777d920bc939b
Sergey Piterman <sergeypiterman@gmail.com>
2018-07-26 22:50:49 -0700

    (feat) adds djikstras algorithm with minHeap

--- DataStructures/Graphs/WeightedGraph.js
+++ DataStructures/Graphs/WeightedGraph.js
@@ -51,14 +51,149 @@ class WeightedGraph {
     dfs(v1, 0)
     return minDistances[v2];
   }
+
+
+  minDistanceEfficient(v1, v2) {
+    let minDistances = {};
+    this.edges.forEach((value, key) => {
+      minDistances[key] = Infinity;
+    })
+
+    let heap = [];
+    heap.push([v1, 0]);
+
+    let g = this; //reference to the graph instance
+
+    while(heap.length > 0) {
+      let [vertex, distance] = removeMin(heap);
+
+      // Only proceed from a vertex if the distance to arrive there is the new shortes
+      if(distance <= minDistances[vertex]) {
+        minDistances[vertex] = distance;
+        let edges = g.edges.get(vertex);
+
+        edges.forEach(e => {
+          let neighbor = e[0];
+          let totalDistance = e[1] + distance;
+          if(totalDistance < minDistances[neighbor]) {
+            minDistances[neighbor] = totalDistance;
+            addToHeap(heap, [neighbor, totalDistance])
+          }
+        })
+      }
+    }
+
+    return minDistances;
+  }
+}
+
+// Heap Methods
+function getParentIndex(index) {
+  if(index === 0) {
+    return 0;
+  }
+  if(index % 2 === 0) {
+    return (index - 2)/2;
+  }
+  return (index - 1)/2;
+}
+
+function bubbleUp(heap) {
+  let currentIndex = heap.length - 1;
+  let current = heap[currentIndex];
+  let parentIndex = getParentIndex(currentIndex);
+  let parent = heap[parentIndex];
+
+
+  while(current[1] < parent[1]) {
+    // Swap both elements
+    [heap[currentIndex], heap[parentIndex]] = [heap[parentIndex], heap[currentIndex]]
+    // Reassign currentIndex, parentIndex, and parent
+    currentIndex = parentIndex;
+    parentIndex = getParentIndex(currentIndex);
+    parent = heap[parentIndex];
+  }
+}
+
+function addToHeap(heap, element) {
+  // Push new element to the end
+  heap.push(element);
+  // Bubble it Up
+  bubbleUp(heap);
+}
+
+function getChildIndices(index) {
+  return [(2 * index) + 1, (2 * index) + 2];
+}
+
+function bubbleDown(heap) {
+  let currentIndex = 0
+  let current = heap[currentIndex];
+  let childIndices = getChildIndices(currentIndex);
+
+  let leftIndex = childIndices[0]
+  let rightIndex = childIndices[1]
+
+  // Given two indices in a heap, return the one with the smaller of two
+  // or return undefined
+  let minChildIndex = findMinChildIndex(heap, leftIndex, rightIndex)
+  let minChild = minChildIndex === undefined ? undefined : heap[minChildIndex];
+
+  while(minChild !== undefined && current[1] > minChild[1]) {
+    // Swap minChild and
+    [heap[currentIndex], heap[minChildIndex]] = [heap[minChildIndex], heap[currentIndex]];
+
+    // Current Index becomes min child's index
+    currentIndex = minChildIndex;
+
+    // Get new left and right child indices
+    [leftIndex, rightIndex] = getChildIndices(currentIndex);
+
+    // New Min Child Index
+    minChildIndex = findMinChildIndex(heap, leftIndex, rightIndex);
+
+    // New min Child
+    minChild = minChildIndex === undefined ? undefined : heap[minChildIndex];
+  }
+
+}
+
+function findMinChildIndex(heap, leftIndex, rightIndex) {
+  let minChildIndex;
+  let leftChild = heap[leftIndex]
+  let rightChild = heap[rightIndex]
+
+  // If Left child is not undefined undefined,
+  if(leftChild !== undefined) {
+    // And the rightChild is undefined
+    if(rightChild === undefined) {
+      // That means there is only one option
+      minChildIndex = leftIndex;
+    } else {
+      // Otherwise choose the smaller of the two
+      minChildIndex = rightChild[1] < leftChild[1] ? rightIndex : leftIndex;
+    }
+  }
+
+  // Returns undefined if there are no children
+  return minChildIndex;
+}
+
+function removeMin(heap) {
+  // Swap first and last element
+  [heap[0], heap[heap.length - 1]] = [heap[heap.length - 1], heap[0]];
+
+  // Remove Last element
+  let min = heap.pop();
+
+  // BubbleDown
+  bubbleDown(heap);
+
+  // Return Smallest element
+  return min;
 }
 
 
-/*
-[[1, 2, 5],
- [2, 3, 6],
- [1, 3, 1]]
-*/
 
 let g = new WeightedGraph()
 
@@ -69,33 +204,34 @@ g.addVertex(4)
 g.addVertex(5)
 g.addVertex(6)
 
-g.addEdge(1, 2, 5)
-g.addEdge(1, 3, 1)
-g.addEdge(1, 5, 8)
-g.addEdge(3, 4, 2)
-g.addEdge(4, 6, 4)
-g.addEdge(6, 1, 7)
-g.addEdge(2, 3, 2)
-g.addEdge(4, 2, 1)
-g.addEdge(2, 5, 2)
-g.addEdge(5, 6, 3)
+// Make undirected Graph
 
-// g.addEdge(1, 3, 1)
-// g.addEdge(1, 2, 10)
+g.addEdge(1, 6, 14)
+g.addEdge(6, 1, 14)
 
-// console.log(g.edges)
-console.log(g.minimumDistance(1, 6))
+g.addEdge(1, 2, 7)
+g.addEdge(2, 1, 7)
 
-/*
-{
-  1: [[2, 10], [3, 1]]
-  2: [[3, 6]]
-  3: [];
-}
-   1
-1 -----> 3
-|       /
-| 5    | 6
-|      |
-2 ------
-*/
+g.addEdge(1, 3, 9)
+g.addEdge(3, 1, 9)
+
+g.addEdge(2, 3, 10)
+g.addEdge(3, 2, 10)
+
+g.addEdge(2, 4, 15)
+g.addEdge(4, 2, 15)
+
+g.addEdge(3, 4, 11)
+g.addEdge(4, 3, 11)
+
+g.addEdge(3, 6, 2)
+g.addEdge(6, 3, 2)
+
+g.addEdge(4, 5, 6)
+g.addEdge(5, 4, 6)
+
+g.addEdge(6, 5, 9)
+g.addEdge(5, 6, 9)
+
+
+console.log(g.minDistanceEfficient(1, 5))
