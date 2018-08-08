86ee40bc12887d93b032752a251a8f6c15920c4f
Sergey Piterman <sergeypiterman@gmail.com>
2018-08-07 21:15:16 -0700

    (feat) begins max population

---
+++ Algorithms/Arrays/maxPopulation.js
@@ -0,0 +1,32 @@
+function birthDeath(arr) {
+
+  let maxPopulation = 0;
+
+}
+
+function getBirths(arr) {
+
+}
+
+let people = [
+  {
+  "name": "john",
+  "birth": 1957,
+  "death": 2001
+  },
+  {
+  "name": "jane",
+  "birth": 1980,
+  "death": 2015
+  },
+  {
+  "name": "stewart",
+  "birth": 1942,
+  "death": 1978
+  },
+  {
+  "name": "mary",
+  "birth": 1960,
+  "death": 2018
+  },
+]
