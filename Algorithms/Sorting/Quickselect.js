41987c22d7469383abcb17d6d43b5d7437705e7b
Sergey Piterman <sergeypiterman@gmail.com>
2018-09-05 18:27:56 -0700

    (fix) fixes quickselect

--- Algorithms/Sorting/Quickselect.js
+++ Algorithms/Sorting/Quickselect.js
@@ -1,26 +1,46 @@
+/*
+wall = 0
+pivot = 5
+end = 5
+start = 0
+current = 0
+k = 1
+
+[3, 1, 6, 4, 9, 8]
+                p
+                c
+             w
+*/
+
+
 function Quickselect(arr, k) {
   let kthLargest;
 
   function recurse(start, end) {
+    if(start > end) return;
+
     let pivot = end;
     let wall = start;
     let current = start;
-    for(current; current <= end; current++) {
+
+    for(current; current < end; current++) {
       if(arr[current] < arr[pivot]) {
-        [arr[current],arr[wall]] = [arr[wall],arr[current]];
+        [arr[current], arr[wall]] = [arr[wall], arr[current]];
         wall++;
       }
     }
-    [arr[pivot],arr[wall]] = [arr[wall],arr[pivot]];
 
-    if(pivot === (arr.length - k)) {
-      kthLargest = arr[pivot];
+    [arr[pivot], arr[wall]] = [arr[wall], arr[pivot]];
+
+    if(wall === (arr.length - k)) {
+      kthLargest = arr[wall];
       return;
     }
-    if(pivot > (arr.length - k)) {
-      recurse(start, pivot - 1);
+
+    if(wall > (arr.length - k)) {
+      recurse(start, wall - 1);
     } else {
-      recurse(pivot + 1, end);
+      recurse(wall + 1, end);
     }
   }
 
@@ -28,4 +48,4 @@ function Quickselect(arr, k) {
   return kthLargest;
 }
 
-console.log(Quickselect([3, 1, 6, 4, 9, 8], 1))
+console.log(Quickselect([3, 1, 6, 4, 9, 8], 5))
